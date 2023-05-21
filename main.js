dd3b31c1e7cb5b3bf64adb115b3533c10dff19a7
Vinicius Lataro <viniciusr.lataro@gmail.com>
2023-05-21 13:41:40 -0300

    Add events and callbacks

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
+// variables
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
 const randomNumber = Math.round(Math.random() * 10);
-
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
 let attemptCount = 1;
 
-function handleClick(event) {
+// events
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", handleResetClick);
+document.addEventListener("keydown", function (e) {
+  if (e.key == "enter") {
+    handleResetClick();
+  }
+});
+
+// callback function
+function handleTryClick(event) {
   event.preventDefault();
 
   const inputNumber = document.querySelector("#inputNumber");
 
   if (Number(inputNumber.value) == randomNumber) {
-    document.querySelector(".screen1").classList.add("hide");
-    document.querySelector(".screen2").classList.remove("hide");
+    screen1.classList.add("hide");
+    screen2.classList.remove("hide");
 
     document.querySelector(
       ".screen2 h2"
@@ -18,5 +32,16 @@ function handleClick(event) {
     console.log(`acertou em ${attemptCount} tentativas`);
   }
 
+  inputNumber.value = "";
   attemptCount++;
+}
+
+function handleResetClick(event) {
+  toggleScreen();
+  attemptCount = 1;
+}
+
+function toggleScreen() {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
 }
